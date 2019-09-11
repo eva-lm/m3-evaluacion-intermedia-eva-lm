67a00d03e398e9aaab3c315e456e1153d6089cc7
eva-lm <eva.lm1810@gmail.com>
2019-09-11 17:10:56 +0200

    gitHub pages

---
+++ docs/precache-manifest.1f1febd8a30b51150f5be955463ae107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0959de83ffc87cbc347b28ec1054f44c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1dd0e39d83acc3872f2",
+    "url": "/static/css/main.1ffb06e1.chunk.css"
+  },
+  {
+    "revision": "0723dc76d54df0260e5c",
+    "url": "/static/js/2.506888dd.chunk.js"
+  },
+  {
+    "revision": "e1dd0e39d83acc3872f2",
+    "url": "/static/js/main.71bd751d.chunk.js"
+  },
+  {
+    "revision": "66f9cd7ad1909cbac7c5",
+    "url": "/static/js/runtime~main.34521c87.js"
+  }
+]);
