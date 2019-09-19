4c3a92f6d600f380fa3975f44528e5e645c554c7
eva-lm <eva.lm1810@gmail.com>
2019-09-19 23:13:12 +0200

    solve issues

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,13 +1,32 @@
-/* import React from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import Pokemon from "./Pokemon";
 
 class PokeList extends React.Component {
-render() {
+  render() {
     return (
-
+      <ul className="list">
+        {this.props.pokemons.map((item, index) => (
+          <li className="pokemonItem" key={`index${index}`}>
+            <Pokemon name={item.name} type={item.types} image={item.url} />
+            <ul className="pokemonTypes">
+              {item.types.map((type, index) => {
+                return (
+                  <li className="pokemonType" key={index}>
+                    <div>{type}</div>
+                  </li>
+                );
+              })}
+            </ul>
+          </li>
+        ))}
+      </ul>
     );
+  }
 }
 
-}
+PokeList.propTypes = {
+  pokemons: PropTypes.array
+};
 
-export default PokeList; */
+export default PokeList;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,26 +1,30 @@
 import React from "react";
-import "../stylesheets/App.css";
 import PropTypes from "prop-types";
 
 class Pokemon extends React.Component {
   render() {
-    const { name, type, image } = this.props;
+    const { name, image, onFavoriteChanges } = this.props;
     return (
-      //<PokeList />
-      <div>
-        <h2>{name}</h2>
-        <p>{type}</p>
-        <div className="imageContainer">
-          <img className="pokemonImage" src={image} alt="imagen pokemon" />
-        </div>
+      <div className="pokemonCard">
+        <label>
+          <input
+            onClick={onFavoriteChanges}
+            type="button"
+            value="&hearts;"
+            name="favorite"
+            id="favorite"
+          ></input>
+        </label>
+
+        <h2 className="PokemonTitle">{name}</h2>
+        <img className="pokemonImage" src={image} alt={name} title={image} />
       </div>
     );
   }
 }
 
 Pokemon.propTypes = {
-  name: PropTypes.string,
-  type: PropTypes.string
+  name: PropTypes.string
 };
 
 export default Pokemon;
