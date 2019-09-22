5f9ba8f93d117c004b2d89b92af028b9691956f9
eva-lm <eva.lm1810@gmail.com>
2019-09-22 17:33:07 +0200

    add gitHub pages

---
+++ docs/build/precache-manifest.c2268fbb07bf6f0a35c03b8dbe8228ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183ef8c8151b3d657a53360f7c3d8787",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afaeef98cd096f6df7da",
+    "url": "./static/css/main.59477733.chunk.css"
+  },
+  {
+    "revision": "7c12c7360b33c289ace1",
+    "url": "./static/js/2.0ebf702a.chunk.js"
+  },
+  {
+    "revision": "afaeef98cd096f6df7da",
+    "url": "./static/js/main.9f8abaf5.chunk.js"
+  },
+  {
+    "revision": "7f53f9b7718af7249a6f",
+    "url": "./static/js/runtime~main.b983ee53.js"
+  }
+]);
