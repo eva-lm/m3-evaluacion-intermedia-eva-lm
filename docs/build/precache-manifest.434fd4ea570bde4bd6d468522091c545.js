2faf9ab50f1b5d2f9cb911916b9d513092406bf9
eva-lm <eva.lm1810@gmail.com>
2019-09-22 18:38:51 +0200

    fix gitHub pages

---
+++ docs/build/precache-manifest.434fd4ea570bde4bd6d468522091c545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0542be9ed8c2e6d986629004effc37ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "332a3af77f0acdcc42bc",
+    "url": "./static/css/main.e19d5ebf.chunk.css"
+  },
+  {
+    "revision": "7c12c7360b33c289ace1",
+    "url": "./static/js/2.0ebf702a.chunk.js"
+  },
+  {
+    "revision": "332a3af77f0acdcc42bc",
+    "url": "./static/js/main.9f8abaf5.chunk.js"
+  },
+  {
+    "revision": "7f53f9b7718af7249a6f",
+    "url": "./static/js/runtime~main.b983ee53.js"
+  },
+  {
+    "revision": "6acb3236c864c103851e9604c1cdf363",
+    "url": "./static/media/yeah.6acb3236.jpg"
+  }
+]);
